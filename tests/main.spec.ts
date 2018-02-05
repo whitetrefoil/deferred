a4a0cf450da1651525f938b0d89ad782d2833efb
WhiteTrefoil <whitetrefoil@gmail.com>
2018-02-05 11:07:01 +0800

    1st working code

---
+++ tests/main.spec.ts
@@ -0,0 +1,17 @@
+import { defer } from '../src/main'
+
+jest.resetModules()
+
+test('resolve', () => {
+  expect.assertions(1)
+  const d = defer()
+  expect(d.promise).resolves.toBe(1)
+  d.resolve(1)
+})
+
+test('reject', () => {
+  expect.assertions(1)
+  const d = defer()
+  expect(d.promise).rejects.toBe(1)
+  d.reject(1)
+})
