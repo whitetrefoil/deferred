68fb9691def6f429f27a9500bef2f1b7cfd23480
WhiteTrefoil <whitetrefoil@gmail.com>
2021-05-07 09:06:54 +0800

    upgrade infrastructure

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 export interface Deferred<T> {
   promise: Promise<T>
-  resolve: (result?: T|PromiseLike<T>) => void
+  resolve: (result: T|PromiseLike<T>) => void
   reject: (reason?: unknown) => void
 }
 
 export function defer<T>(): Deferred<T> {
-  let resolve!: (result?: T|PromiseLike<T>) => void
+  let resolve!: (result: T|PromiseLike<T>) => void
   let reject!: (reason?: unknown) => void
   const promise: Promise<T> = new Promise<T>((_resolve, _reject) => {
     resolve = _resolve
